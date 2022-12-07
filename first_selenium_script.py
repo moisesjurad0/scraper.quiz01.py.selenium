53e61767b4707cff6b7897194fd7337ae638661d
moisesJurad0 <moises003@gmail.com>
2022-12-07 08:56:42 -0500

    15 avance

--- first_selenium_script.py
+++ first_selenium_script.py
@@ -3,9 +3,9 @@
 Returns:
     _type_: _description_
 """
-
 import configparser
 import logging
+import sys
 import time
 
 from bs4 import BeautifulSoup
@@ -15,6 +15,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 # from threading import Thread
+logging.basicConfig(filename='myfirstlog.log', level=logging.DEBUG,
+                    format='%(asctime)s | %(name)s | %(levelname)s | %(message)s')
 logger = logging.getLogger('scrapping01')
 logger.setLevel(logging.INFO)
 logger.info('inicio')
@@ -22,167 +24,154 @@ logger.info('inicio')
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-driver_path = config['DEFAULT']['driver_path']
-brave_path = config['DEFAULT']['brave_path']
-exam_url = config['DEFAULT']['exam_url']
-quiz_url = config['DEFAULT']['quiz_url']
 
-service = Service(driver_path)
-options = Options()
-options.binary_location = brave_path
-# ua = UserAgent()
-# userAgent = ua.random
-# log.info(userAgent)
-# opts_chrome.add_argument(f'user-agent={userAgent}')
-# opts_chrome.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36')
-# opts_chrome.add_argument("headless")
-options.add_argument("start-maximized")
-options.add_argument("--incognito")
-options.add_argument("disable-infobars")
-options.add_argument("--disable-extensions")
-options.add_argument("disable-extensions")
-options.add_argument("allow-insecure-localhost")
-options.add_argument("ignore-ssl-errors=yes")
-options.add_argument("ignore-certificate-errors")
+def main():
+    driver_path = config['DEFAULT']['driver_path']
+    brave_path = config['DEFAULT']['brave_path']
+    exam_url = config['DEFAULT']['exam_url']
+    quiz_url = config['DEFAULT']['quiz_url']
 
-# Create new Instance of Chrome
-browser = webdriver.Chrome(service=service, options=options)
+    service = Service(driver_path)
+    options = Options()
+    options.binary_location = brave_path
+    # ua = UserAgent()
+    # userAgent = ua.random
+    # log.info(userAgent)
+    # opts_chrome.add_argument(f'user-agent={userAgent}')
+    # opts_chrome.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36')
+    # opts_chrome.add_argument("headless")
+    options.add_argument("start-maximized")
+    options.add_argument("--incognito")
+    options.add_argument("disable-infobars")
+    options.add_argument("--disable-extensions")
+    options.add_argument("disable-extensions")
+    options.add_argument("allow-insecure-localhost")
+    options.add_argument("ignore-ssl-errors=yes")
+    options.add_argument("ignore-certificate-errors")
+    options.add_argument("--log-level=3")
 
-browser.get(quiz_url)
-browser.implicitly_wait(5)
-print(f'titulo de la pagina => {browser.title}')
-logger.info(f'titulo de la pagina => {browser.title}')
+    # Create new Instance of Chrome
+    browser = webdriver.Chrome(service=service, options=options)
 
+    browser.get(quiz_url)
+    browser.implicitly_wait(5)
+    print(f'titulo de la pagina => {browser.title}')
+    logger.info(f'titulo de la pagina => {browser.title}')
 
-def safe_find_element(browser_in, the_type, selector):
-    """Buscar elemento de manera segura.
+    def safe_find_element(browser_in, the_type, selector):
+        """Buscar elemento de manera segura.
 
-    Args:
-        browser_in (_type_): _description_
-        the_type (_type_): _description_
-        selector (_type_): _description_
+        Args:
+            browser_in (_type_): _description_
+            the_type (_type_): _description_
+            selector (_type_): _description_
 
-    Returns:
-        _type_: _description_
-    """
-    obj_element = None
-    try:
-        obj_element = browser_in.find_element(
-            the_type,
-            selector)
-    except Exception as e:
-        logger.warning(e, exc_info=True)
-    return obj_element
+        Returns:
+            _type_: _description_
+        """
+        obj_element = None
+        try:
+            obj_element = browser_in.find_element(
+                the_type,
+                selector)
+        except Exception as e:
+            logger.warning(e, exc_info=True)
+        return obj_element
 
-
-btn_continue = safe_find_element(
-    browser, By.XPATH,
-    '//*[@id="app"]/ion-app/div/div[1]/ion-content/div/div[3]/ion-button',
-)
-btn_continue.click()
-
-btn_continue2 = safe_find_element(
-    browser, By.XPATH,
-    '//*[@id="app"]/ion-app/div/div[1]/ion-content/div/div[3]/ion-button[2]')
-btn_continue2.click()
-
-contadorDePreguntas = 0
-
-# ANSWERING QUESTIONS WITH REAL OR UNREAL DATA
-while True:
-    div_question = safe_find_element(
+    btn_continue = safe_find_element(
         browser, By.XPATH,
-        '/html/body/div/ion-app/div/div[1]/ion-content/div/div[2]/div/div[4]/div/ion-card/ion-card-content/div/ion-list/ion-list-header/div/div')
+        '//*[@id="app"]/ion-app/div/div[1]/ion-content/div/div[3]/ion-button',
+    )
+    btn_continue.click()
 
-    if div_question:
-        print(div_question.text)
-        contadorDePreguntas += 1
+    btn_continue2 = safe_find_element(
+        browser, By.XPATH,
+        '//*[@id="app"]/ion-app/div/div[1]/ion-content/div/div[3]/ion-button[2]')
+    btn_continue2.click()
 
-        # CHECKBOXES or RADIO BUTTONS
-        a_block_of_answers = browser.find_elements(
-            By.XPATH,
-            '//*[starts-with(@id, "question-")]/ion-card/ion-card-content/div/ion-list/ion-item'
-            ' | '
-            '//*[starts-with(@id, "question-")]/ion-card/ion-card-content/div/ion-list/ion-radio-group/ion-item')
+    contadorDePreguntas = 0
 
-        if contadorDePreguntas == 29:
-            logger.info('29')
-            print('29')
+    # ANSWERING QUESTIONS WITH REAL OR UNREAL DATA
+    while True:
+        div_question = safe_find_element(
+            browser, By.XPATH,
+            '/html/body/div/ion-app/div/div[1]/ion-content/div/div[2]/div/div[4]/div/ion-card/ion-card-content/div/ion-list/ion-list-header/div/div')
 
-        for answer in a_block_of_answers:
-            browser.execute_script(
-                "arguments[0].scrollIntoView(true);", answer)
-            answer.click()
+        if div_question:
+            print(div_question.text)
+            contadorDePreguntas += 1
 
-        btn_next_or_finish_now = browser.find_element(
-            By.CSS_SELECTOR,
-            'ion-button[data-cy="continue-btn"]'
-            ','
-            'ion-button[data-cy="finish-btn"]')
-
-        attribute_data_cy = btn_next_or_finish_now.get_attribute('data-cy')
-        print(f'atributo_data_cy->{attribute_data_cy}')
-        logger.info(f'atributo_data_cy->{attribute_data_cy}')
-
-        btn_next_or_finish_now.click()
-        time.sleep(1.5)
-        # if btn_next_or_finish.text == 'Finish now':
-
-        if attribute_data_cy == 'finish-btn':
-            # seccion del boton "Confirm finish now"
-            btn_confirm_finish_now = browser.find_element(
+            # CHECKBOXES or RADIO BUTTONS
+            a_block_of_answers = browser.find_elements(
                 By.XPATH,
-                '//*[@id="test_confirm_finish"]')
-            btn_confirm_finish_now.click()
-            time.sleep(2)
+                '//*[starts-with(@id, "question-")]/ion-card/ion-card-content/div/ion-list/ion-item'
+                ' | '
+                '//*[starts-with(@id, "question-")]/ion-card/ion-card-content/div/ion-list/ion-radio-group/ion-item')
+
+            if contadorDePreguntas == 30:
+                logger.info('30')
+                print('30')
+
+            for answer in a_block_of_answers:
+                browser.execute_script(
+                    "arguments[0].scrollIntoView(true);", answer)
+                answer.click()
+
+            btn_next_or_finish_now = browser.find_element(
+                By.CSS_SELECTOR,
+                'ion-button[data-cy="continue-btn"]'
+                ','
+                'ion-button[data-cy="finish-btn"]')
+
+            attribute_data_cy = btn_next_or_finish_now.get_attribute('data-cy')
+            print(f'atributo_data_cy->{attribute_data_cy}')
+            logger.info(f'atributo_data_cy->{attribute_data_cy}')
+
+            btn_next_or_finish_now.click()
+            time.sleep(1.5)
+            # if btn_next_or_finish.text == 'Finish now':
+
+            if attribute_data_cy == 'finish-btn':
+                # seccion del boton "Confirm finish now"
+                btn_confirm_finish_now = browser.find_element(
+                    By.XPATH,
+                    '//*[@id="test_confirm_finish"]')
+                btn_confirm_finish_now.click()
+                time.sleep(2)
+                break
+        else:
             break
-    else:
-        break
 
-# SCANNING CORRECT ANSWERS FROM FEEDBACK PAGE
-page_source = browser.page_source
-soup = BeautifulSoup(page_source, 'lxml')
+    # SCANNING CORRECT ANSWERS FROM FEEDBACK PAGE
+    page_source = browser.page_source
+    soup = BeautifulSoup(page_source, 'lxml')
 
-feedbacks = soup.find_all('div', class_='feedback')
-for feedback in feedbacks:
-    print(feedback.text)
-    # just to separate the feedbacks
-    #print('-------------------------------------')
-    # correctos = feedback.find_all_next('ion-icon', class_='circular-tick-holo')  # 'circular-x'
+    feedbacks = soup.find_all('div', class_='feedback')
+    for feedback in feedbacks:
+        print(feedback.text)
+        # just to separate the feedbacks
+        # print('-------------------------------------')
+        # correctos = feedback.find_all_next('ion-icon', class_='circular-tick-holo')  # 'circular-x'
 
-    #para checkBox es circular-tick, para radio es circular-tick-holo
-    correctosCheck=feedback.find_all('ion-icon', class_='circular-tick')
-    correctosRadio=feedback.find_all('ion-icon', class_='circular-tick-holo')
+        # para checkBox es circular-tick, para radio es circular-tick-holo
+        correctosCheck = feedback.find_all('ion-icon', class_='circular-tick')
+        correctosRadio = feedback.find_all('ion-icon', class_='circular-tick-holo')
 
-    if correctosCheck:
-        for correcto in correctosCheck:
-            print(correcto)
-    elif correctosRadio:
-        for correcto in correctosRadio:
-            print(correcto)
+        if correctosCheck:
+            for correcto in correctosCheck:
+                print(correcto)
+        elif correctosRadio:
+            for correcto in correctosRadio:
+                print(correcto)
 
-
-
+    # browser.quit()
+    # browser.close()
 
 
-# # byid
-# cart = safe_find_element(browser, By.ID, 'site-header-cart')
-# print(f'site-header-cart => {cart}')
-# print(f'tipo => {type(cart)}')
-# cart_text = cart.text
-# print(f'cart_text => {cart_text}')
-
-# # byid
-# search_field = browser.find_element(
-#     By.ID, 'woocommerce-product-search-field-0')
-# search_field.send_keys('Hoddie')
-# search_field.send_keys(Keys.ENTER)
-
-# # byCSSselector
-# # miMenuMyAccount = browser.find_element(By.CSS_SELECTOR, '#site-navigation > div:nth-child(2) > ul > li.page_item.page-item-9.current_page_item > a')
-# miMenuMyAccount = browser.find_element(
-#     By.XPATH, '//*[@id="site-navigation"]/div[1]/ul/li[4]')
-# miMenuMyAccount.click()
-
-# browser.quit()
-# browser.close()
+if __name__ == "__main__":
+    try:
+        main()
+        sys.exit(0)
+    except Exception as e:
+        logger.error(str(e), exc_info=True)
+        sys.exit(1)
