1909ca16f0624d6118230c376d976691cf1151b3
Moisés Jurado <moises003@gmail.com>
2023-10-01 14:22:48 -0500

    fastapi and threads | cli

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
 """main script module without package."""
+import datetime
 import logging
-import sys
+import threading
 from pathlib import Path
 
-import typer
+import uvicorn
+from fastapi import Depends, FastAPI  # , HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 from quiz01 import config, quiz01_scraper
+
+# from fastapi.encoders import jsonable_encoder
+# from fastapi.responses import JSONResponse
 
 script_path = Path(__file__).absolute()
 script_dir = Path(__file__).parent.absolute()
@@ -14,35 +20,125 @@ log_folder.mkdir(parents=True, exist_ok=True)
 
 logging.basicConfig(
     filename=log_folder /
-    f'quiz01scraper{config.currentDT.strftime("%Y%m%d%H%M%S")}.log',
+    f'quiz01scraper.api{config.currentDT.strftime("%Y%m%d%H%M%S")}.log',
     level=logging.INFO,
     format=(
         '%(asctime)s | %(name)s | %(levelname)s |'
         ' [%(filename)s:%(lineno)d] | %(message)s'))
 
 
-def main(exam_number: int):
+description = """
+quiz01.scraper.BOT-API helps you do operations with the BOT. 🚀
+
+## default
+
+You will be able to:
+
+* **Run questions** Run the bot and get a message if its bussys.
+"""
+
+app = FastAPI(
+    title="quiz01.scraper.BOT-API",
+    description=description,
+    summary="do run of the bot.",
+    version="0.0.1",
+    contact={
+        "name": "moisesjurad0",
+        "url": "https://linktr.ee/moisesjurad0",
+
+    },
+    # root_path=f'/Prod'
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+sessions = {}
+
+
+@app.get("/")
+def read_root():
+    """Funcion que devuelve un estado plano OK definido para prueba simple."""
+    return {"API_status": "OK"}
+
+
+def get_session_id():
+    """_summary_.
+
+    Returns:
+        _type_: _description_
+    """
+    return threading.current_thread().ident
+
+
+@app.post("/run/{exam_number}")
+def run(exam_number: int, session_id: int = Depends(get_session_id)):
+    """Run the bot.
+
+    Args:
+        exam_number (int): exam number to be interacted/scraped
+
+    Returns:
+        _type_: _description_
+    """
+    print('run-start')
+    logging.info('run-start')
+
+    hilo = threading.Thread(target=main, args=(exam_number,))
+    hilo.start()
+
+    print('run-ending')
+    logging.info('run-ending')
+    return {"message": "Evaluating task in the background"}
+
+
+@app.get("/check/")
+def check():
+    """Check the bot.
+
+    Args:
+        exam_number (int): exam number to be interacted/scraped
+
+    Returns:
+        _type_: _description_
+    """
+    return sessions
+
+
+def main(exam_number: int, session_id: int = Depends(get_session_id)):
     """Quiz01-Scraper => Scrape CLI.
 
     Args:
-        exam_number (int, optional): Parametro para decidir que examen ejecutar. Dejarlo vacio usa el default del config.ini. Defaults to 1.
+        exam_number (int): Parametro para decidir que examen ejecutar. Dejarlo vacio usa el default del config.ini. Defaults to 1.
+        session_id (int, optional): _description_. Defaults to Depends(get_session_id).
     """
-    logging.info(f'{config.STARS_SEPARATOR} PARAMETROS DE ENTRADA => '
-                 f'exam_number:{exam_number}'
-                 f' {config.STARS_SEPARATOR}')
+    logging.info('m01.START')
+    print('m01.START')
+
+    sessions[exam_number] = {
+        "session_id": session_id,
+        "start_time": datetime.datetime.now()}
+
+    # test 1
     quiz01_scraper.do_scraping(exam_number)
 
+    # test 2
+    # import time
+    # time.sleep(60)
 
-# if __name__ == "__main__":
-#     typer.run(main)
+    # test 3
+    # for i in range(10):
+    #     print(f'subtarea {i}')
+
+    sessions.pop(exam_number)
+
+    logging.info('m01.END')
+    print('m01.END')
 
 
 if __name__ == "__main__":
-    try:
-        logging.info('START')
-        typer.run(main)
-        logging.info('END')
-        sys.exit(0)
-    except Exception as e:
-        logging.error(str(e), exc_info=True)
-        sys.exit(1)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
